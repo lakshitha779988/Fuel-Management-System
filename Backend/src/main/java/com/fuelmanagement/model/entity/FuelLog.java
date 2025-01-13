922a210130189d265793c878d983810da41bfe4b
Thagya <kavindithagya21@gmail.com>
2025-01-13 17:50:31 +0530

    Add FuelLog entity class and FuelLogRepository interface

---
+++ Backend/src/main/java/com/fuelmanagement/model/entity/FuelLog.java
@@ -0,0 +1,91 @@
+
+package com.fuelmanagement.model.entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class FuelLog  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date createdAt;
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date transactionTime;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "station_id", nullable = false)
+    private FuelStation fuelStation;
+
+    @Column(nullable = false)
+    private Float fuelAmount;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getTransactionTime() {
+        return transactionTime;
+    }
+
+    public void setTransactionTime(Date transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public FuelStation getFuelStation() {
+        return fuelStation;
+    }
+
+    public void setFuelStation(FuelStation fuelStation) {
+        this.fuelStation = fuelStation;
+    }
+
+    public Float getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public void setFuelAmount(Float fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+}
