f71cedcc2f3abcb87421f9b72220a60c41b99387
lakshitha779988 <madusha-se21033@stu.kln.ac.lk>
2025-01-16 08:54:35 +0530

    authenication

---
+++ Backend/src/main/java/com/fuelmanagement/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.fuelmanagement.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.authorizeHttpRequests(request -> request
+                .requestMatchers("register","logging","users/{id}" ,"vehicles/types" ,"api/auth/register")
+                .permitAll()
+                .anyRequest().authenticated());
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return httpSecurity.build();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
---
+++ Backend/src/main/java/com/fuelmanagement/model/dto/request/RegistrationRequest.java
@@ -0,0 +1,131 @@
+package com.fuelmanagement.model.dto.request;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RegistrationRequest {
+
+
+    private String mobileNumber;
+
+
+    private String password;
+
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+
+
+    private String nationalId;
+
+
+    private String chaseNumber;
+
+
+    private String vehicleNumber;
+
+    private Long vehicleTypeId;
+
+    private String fuelType;
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getChaseNumber() {
+        return chaseNumber;
+    }
+
+    public void setChaseNumber(String chaseNumber) {
+        this.chaseNumber = chaseNumber;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String registrationNumber) {
+        this.vehicleNumber = registrationNumber;
+    }
+
+    public Long getVehicleTypeId() {
+        return vehicleTypeId;
+    }
+
+    public void setVehicleTypeId(Long vehicleTypeId) {
+        this.vehicleTypeId = vehicleTypeId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistrationRequest{" +
+                "mobileNumber='" + mobileNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", nationalId='" + nationalId + '\'' +
+                ", chaseNumber='" + chaseNumber + '\'' +
+                ", vehicleNumber='" + vehicleNumber + '\'' +
+                ", vehicleTypeId=" + vehicleTypeId +
+                ", fuelType='" + fuelType + '\'' +
+                '}';
+    }
+}
