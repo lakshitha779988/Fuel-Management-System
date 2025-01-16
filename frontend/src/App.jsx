a4a694f2bb0c63d69598ecd0916f81c2ab8836ba
lakshitha779988 <madusha-se21033@stu.kln.ac.lk>
2025-01-16 12:05:44 +0530

    initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+  
+    <h1 class="text-4xl font-bold text-center text-blue-500 mt-4">
+    Welcome to fuil Management System 
+  </h1>
+  
+  );
+}
+
+export default App;
+
