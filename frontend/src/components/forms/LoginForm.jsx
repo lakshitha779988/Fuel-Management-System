83659c292946ce7647576e2fac2569189c77294b
Buddhikamadumali <146803876+Buddhikamadumali@users.noreply.github.com>
2025-01-18 14:52:38 +0530

    Frontend-update Login form

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
 
 function LoginForm() {
+    const [otpSent, setOtpSent] = useState(false);
+    const [otp, setOtp] = useState('');
+    const [mobileNumber,setMobileNumber] = useState('');
+
+
   return (
     <div>
       <form>
-            <Input label="Mobile Number"
+            <Input label="Mobile Number" 
             type="text"
-            placeholder="Enter your mobile number"/>
+            value={mobileNumber}
+            placeholder="Enter your mobile number"
+            onChange={(e) => setMobileNumber(e.target.value)}/>
             <button>Sent OTP</button>
+            {otpSent && (
+                <>
+                   <label htmlFor="otp">OTP</label>
+                    <input
+                    id="otp"
+                    type="text"
+                    placeholder="Enter the OTP"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                    />
+                    <button>Verify OTP</button>
+
+                </>
+            )}
         </form>
     </div>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
