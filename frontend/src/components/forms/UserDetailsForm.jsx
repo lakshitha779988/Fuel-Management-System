94d8d6a18361530d5a82f23ce290ce4c2614f14c
Thagya <kavindithagya21@gmail.com>
2025-01-18 14:51:22 +0530

    Frontend-update UserDetailsForm

--- frontend/src/components/forms/UserDetailsForm.jsx
+++ frontend/src/components/forms/UserDetailsForm.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 function UserDetailsForm() {
   return (
-    <div>UserDetailsForm</div>
+    <div>
+        <form>
+        
+  
+
+  
+      <><h3>User Details</h3><Input label="First Name" type="text" placeholder="Ex:Saman" />
+      <Input label="Last Name" type="text" placeholder="Ex: Perera" />
+      <Input label="Address" type="text" placeholder="Ex: 399/8, Station Road, Colombo" />
+      <Input label="Mobile Number" type="text" placeholder="Ex: 077 123 4567" />
+      <Input label="NIC" type="text" placeholder="Ex: 200134587570" />
+      </>
+
+  </form>
+  </div>
   )
+
 }
 
 export default UserDetailsForm
