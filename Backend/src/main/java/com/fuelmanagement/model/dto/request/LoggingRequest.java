7e38accab0b06d88acfd0e86312205e427926735
lakshitha779988 <madusha-se21033@stu.kln.ac.lk>
2025-01-18 14:08:56 +0530

    create logging request DTO

--- Backend/src/main/java/com/fuelmanagement/model/dto/request/LoggingRequest.java
+++ Backend/src/main/java/com/fuelmanagement/model/dto/request/LoggingRequest.java
@@ -5,4 +5,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LoggingRequest {
+   private String mobileNumber;
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
