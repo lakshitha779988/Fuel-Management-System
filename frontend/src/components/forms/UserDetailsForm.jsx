1219362575a27c4efd1c5507924ec9e6f0244734
Thagya <kavindithagya21@gmail.com>
2025-01-18 15:39:53 +0530

    Frontend-Update UserDetailsForm

--- frontend/src/components/forms/UserDetailsForm.jsx
+++ frontend/src/components/forms/UserDetailsForm.jsx
@@ -6,13 +6,25 @@ function UserDetailsForm() {
         <form>
         
   
+      <>
+      
+      <h3>User Details</h3>
 
-  
-      <><h3>User Details</h3><Input label="First Name" type="text" placeholder="Ex:Saman" />
-      <Input label="Last Name" type="text" placeholder="Ex: Perera" />
-      <Input label="Address" type="text" placeholder="Ex: 399/8, Station Road, Colombo" />
-      <Input label="Mobile Number" type="text" placeholder="Ex: 077 123 4567" />
-      <Input label="NIC" type="text" placeholder="Ex: 200134587570" />
+      <label htmlFor="First Name" >First Name</label>
+      <input id="First Name" type="text" placeholder="Ex:Saman" />
+
+      <label htmlFor="Last Name" >Last Name</label>
+      <input id="Last Name" type="text" placeholder="Ex: Perera" />
+
+      <label htmlFor="Address" >Address</label>
+      <input id="Address" type="text" placeholder="Ex: 399/8, Station Road, Colombo" />
+
+      <label htmlFor="NIC" >NIC</label>
+      <input id="NIC" type="text" placeholder="Ex: 200134587570" />
+
+      <label htmlFor="Mobile Number" >Mobile Number</label>
+      <input id="Mobile Number" type="text" placeholder="Ex: 077 123 4567" />
+
       </>
 
   </form>
