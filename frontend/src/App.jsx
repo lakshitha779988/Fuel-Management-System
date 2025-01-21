3f7b1035e01dcc33d313cad6c8b6e83335d1f46b
Buddhikamadumali <146803876+Buddhikamadumali@users.noreply.github.com>
2025-01-21 17:35:00 +0530

    Create dashboard template and configure JWT authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/forms/LoginForm';
 import UserDetailsForm from './components/forms/UserDetailsForm';
+import TestDashboard from './components/Pages/TestDashboard';
 
 function App() {
   return (
     <Router>
       <>
-        <h1 className="text-4xl font-bold text-center text-blue-500 mt-4">
+        <h1 className="text-4xl font-bold text-center text-red-500 mt-4">
           Welcome to Fuel Management System
         </h1>
         <Routes>
           <Route path="/login" element={<LoginForm />} />
           <Route path="/UserDetails" element={<UserDetailsForm />} />
+          <Route path="/dashboard" element={<TestDashboard/>} />
+          
         </Routes>
       </>
     </Router>
