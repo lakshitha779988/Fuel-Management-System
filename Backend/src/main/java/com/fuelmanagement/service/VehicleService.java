e3323726cdac729211cbee0573997a139cadaba3
Lakshitha Madushan <136601617+lakshitha779988@users.noreply.github.com>
2025-01-23 11:57:19 +0530

    Update VehicleService.java

--- Backend/src/main/java/com/fuelmanagement/service/VehicleService.java
+++ Backend/src/main/java/com/fuelmanagement/service/VehicleService.java
@@ -5,7 +5,7 @@ import com.fuelmanagement.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-    @Service
+@Service
     public class VehicleService {
         @Autowired
         private VehicleRepository vehicleRepository;
