d39759ba5e9fea04a1907846ae6c2942963a5993
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-01-24 14:15:48 +0530

    Created "fuelUsageForEachVehicle" method in ReportController class.

--- Backend/src/main/java/com/fuelmanagement/controller/ReportController.java
+++ Backend/src/main/java/com/fuelmanagement/controller/ReportController.java
@@ -1,4 +1,15 @@
 package com.fuelmanagement.controller;
 
+
+import com.fuelmanagement.repository.mysql.FuelLogRepository;
+import com.fuelmanagement.repository.mysql.VehicleTypeRepository;
+
 public class ReportController {
+
+
+
+    public Float fuelUsageForEachVehicle(String registrationNumber) {
+
+
+
 }
