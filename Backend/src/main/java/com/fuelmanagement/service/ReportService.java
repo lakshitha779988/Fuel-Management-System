6918f1277142e321a81da5d3748805377e20774f
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-01-24 14:25:13 +0530

    Created " FuelUsageForEachVehicle" method in "ReportService" class.

--- Backend/src/main/java/com/fuelmanagement/service/ReportService.java
+++ Backend/src/main/java/com/fuelmanagement/service/ReportService.java
@@ -1,4 +1,15 @@
 package com.fuelmanagement.service;
 
+
+import com.fuelmanagement.repository.mysql.FuelLogRepository;
+
 public class ReportService {
+
+FuelLogRepository fuelLogRepository;
+
+public Float FuelUsageForEachVehicle(String registrationNumber){
+
+}
+
+
 }
