aa4bd30cb56bac1e01482dbb3d4f1c290053bd0b
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-01-24 14:19:51 +0530

    Completed "fuelUsageForEachVehicle" method in "ReportController" class.

--- Backend/src/main/java/com/fuelmanagement/controller/ReportController.java
+++ Backend/src/main/java/com/fuelmanagement/controller/ReportController.java
@@ -3,13 +3,16 @@ package com.fuelmanagement.controller;
 
 import com.fuelmanagement.repository.mysql.FuelLogRepository;
 import com.fuelmanagement.repository.mysql.VehicleTypeRepository;
+import com.fuelmanagement.service.ReportService;
 
 public class ReportController {
 
-
+    ReportService reportService;
 
     public Float fuelUsageForEachVehicle(String registrationNumber) {
 
+        Float FuelUsage = reportService.FuelUsageForEachVehicle(registrationNumber)
 
-
+        return (FuelUsage);
+    }
 }
