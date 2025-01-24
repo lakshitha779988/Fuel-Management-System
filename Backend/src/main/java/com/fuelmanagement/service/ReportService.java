04f0987c526dc231ba47095b38882d8e04de688c
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-01-24 14:58:03 +0530

    Got All variable to a List variable.

--- Backend/src/main/java/com/fuelmanagement/service/ReportService.java
+++ Backend/src/main/java/com/fuelmanagement/service/ReportService.java
@@ -1,13 +1,20 @@
 package com.fuelmanagement.service;
 
 
+import com.fuelmanagement.model.entity.mysql.FuelLog;
 import com.fuelmanagement.repository.mysql.FuelLogRepository;
+
+import java.util.List;
 
 public class ReportService {
 
 FuelLogRepository fuelLogRepository;
 
 public Float FuelUsageForEachVehicle(String registrationNumber){
+
+     List<FuelLog> allTransactions = fuelLogRepository.findByVehicle_VehicleRegistrationNumber(registrationNumber);
+
+
 
 }
 
