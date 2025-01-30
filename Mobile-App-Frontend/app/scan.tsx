1b009821470431bceece0c86577cf6e7fd87ad9b
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-01-30 16:04:11 +0530

    Take permission to access Camera.

--- Mobile-App-Frontend/app/scan.tsx
+++ Mobile-App-Frontend/app/scan.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Camera, CameraView} from "expo-camera";
 import {Platform, SafeAreaView,StatusBar} from "react-native";
+import {useRouter} from "expo-router";
 
 
 export default function ScanScreen() {
+
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+    const [scanning,SetScanning] = useState(false);
+    const router = useRouter();
+    const qrLock = useRef(false);
+
+    useEffect(() => {
+        const requestPermission = async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === "granted");
+        };
+        requestPermission();
+    }, []);
+
+
+
 
 
 
