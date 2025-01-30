618908ac316cbbe528ca53e0e5cded8a7242d97b
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2025-01-30 15:54:47 +0530

    Created visual components of scan.tsx file

--- Mobile-App-Frontend/app/scan.tsx
+++ Mobile-App-Frontend/app/scan.tsx
@@ -1,5 +1,22 @@
+import React from "react";
+import {Camera, CameraView} from "expo-camera";
+import {Platform, SafeAreaView,StatusBar} from "react-native";
+
 
 export default function ScanScreen() {
 
 
+
+    return(
+
+        <SafeAreaView style={StyleSheet.absoluteFillObject}>
+
+            {Platform.OS === "android" ? <StatusBar hidden />:  null}
+
+        <CameraView
+            style={StyleSheet.absoluteFillObject}
+            onBarcodeScanned = {scanning ? undefined : handleBarCodeScanned}
+           />
+        </SafeAreaView>
+    );
 }
