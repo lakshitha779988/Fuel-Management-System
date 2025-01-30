dda3386c5aceb5bd1cbd1bcb17d4e5a2c1b5525b
lakshitha779988 <madusha-se21033@stu.kln.ac.lk>
2025-01-30 13:36:26 +0530

    Add schedule service to reset FuelQuotaTracker in every monday of every user

--- Backend/src/main/java/com/fuelmanagement/repository/mysql/FuelQuotaTrackerRepository.java
+++ Backend/src/main/java/com/fuelmanagement/repository/mysql/FuelQuotaTrackerRepository.java
@@ -1,9 +1,18 @@
 package com.fuelmanagement.repository.mysql;
 
 import com.fuelmanagement.model.entity.mysql.FuelQuotaTracker;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FuelQuotaTrackerRepository extends JpaRepository<FuelQuotaTracker,Long> {
+public interface FuelQuotaTrackerRepository extends JpaRepository<FuelQuotaTracker, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE fuel_quota_tracker SET existing_fuel = weekly_consumption, reset_date = CURDATE()", nativeQuery = true)
+    int resetFuelQuota();
+
 }
