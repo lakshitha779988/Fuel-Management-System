4e20d71fb16f5d46e61189df8514988d40975736
Praveen Dedigama <146774479+Praveendedigama@users.noreply.github.com>
2025-01-31 12:20:00 +0530

    Create UpdateScreen.tsx and insiate it

---
+++ Mobile-App-Frontend/app/(root)/(tabs)/UpdateScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function UpdateScreen() {
+  return (
+    <View>
+      <Text>
+        updateScreen
+      </Text>
+    </View>
+  );
+}
